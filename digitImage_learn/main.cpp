052586f9f7441e5f3d06f89b9a37b93bf07839eb
zhangxun <15025689012@163.com>
2016-11-03 13:43:16 +0800

    Initial Commit

---
+++ digitImage_learn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  digitImage_learn
+//
+//  Created by zhangxun on 16/11/3.
+//  Copyright © 2016年 zx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
